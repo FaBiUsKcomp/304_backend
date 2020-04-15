6b533c6bb525dd045652a19f9000dd069fb79dbc
Fábio Oliveira <fabio.oliveira2@estudante.ufla.br>
2020-04-15 09:35:37 -0300

    :hammer: Adiciona Segurança.
    Descrição: Adicão de Passport nas Rotas para melhorar a segurança da API.

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -10,14 +10,21 @@ module.exports = app => {
     app.post('/validate', app.api.auth.validate)
 
     //USER
+    app.route('/user').all(app.config.passport.authenticate()) //Autorization
     app.get('/user', app.api.user.readUsers)
     app.get('/user/:id', app.api.user.readAUser)
     app.post('/user', app.api.user.createUser)
     app.put('/user', app.api.user.updateUser)
     app.delete('/user/:id', app.api.user.deleteUser)
 
-    //CHAT
+    //DISPENSATION
+    app.get('/dispensation', app.api.dispensation.readProducts)
+    app.post('/dispensation', app.api.dispensation.createProduct)
+    app.post('/dispensation/missing', app.api.dispensation.createMissingProduct)
+    app.get('/dispensation/missing', app.api.dispensation.readMissingProducts)
 
+    //CHAT
+    app.route('/chat').all(app.config.passport.authenticate())
     app.get('/chat', app.api.chat.readChat)
     app.post('/chat', app.api.chat.createMessage)
 }
