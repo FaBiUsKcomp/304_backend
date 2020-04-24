8da485a5eb68c5920e60f82aa50c1bc964557569
Fábio Oliveira <fabio.oliveira2@estudante.ufla.br>
2020-04-23 22:38:28 -0300

    :hammer: Adiciona Model da API de Limpeza.

--- backend/model/CleaningModel.js
+++ backend/model/CleaningModel.js
@@ -1,2 +1,38 @@
 const { Schema, model } = require('mongoose')
 
+const week = new Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    sunday:{
+        type: String,
+        required: true
+    }, 
+    monday: {
+        type: String,
+        required: true
+    }, 
+    tuesday:{
+        type: String,
+        required: true
+    }, 
+    wednesday:{
+        type: String,
+        required: true
+    }, 
+    thursday: {
+        type: String,
+        required: true
+    }, 
+    friday: {
+        type: String,
+        required: true
+    }, 
+    saturday: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = model('week', week)
